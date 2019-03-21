da5f1e21459024fb46a63a55fe20740cde30d9c7
Tianyi6679 <timtianyima@gmail.com>
2019-03-20 22:17:30 -0700

    update util.py

--- util.py
+++ util.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-def softmax(X, theta):
-    ps = np.exp(X-np.max(X))
-    ps /= np.sum(ps)
-    return ps
+def softmax(X):
+    ps = np.exp(X)
+    sum = np.sum(ps, axis = 1)
+    ps /= sum.reshape(-1,1)
+    return sum, ps
 
 def normalize(X):
     return X/X.sum(axis=1).reshape(len(X),1)
